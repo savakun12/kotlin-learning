349cd385831dcc2ba6c63ad66eac63de9b8a2f7d
jeda ngoding <46700670+savakun12@users.noreply.github.com>
2024-10-06 21:18:15 +0700

    Update kotlin.kt

--- kotlin.kt
+++ kotlin.kt
@@ -15,4 +15,16 @@ fun tambah(a: Int, b: Int): Int {
 //Fungsi dengan single-line expression
 fun kali(a: Int, b: Int) = a * b
 
-
+##Kondisional
+fun nilaiUjian(nilai: Int): String {
+    return if (nilai >= 90) {
+        "A"
+    } else if (nilai >= 80) {
+        "B"
+    } else if (nilai >= 70) {
+        "C"
+    } else {
+        "D"
+    }
+}
+        
