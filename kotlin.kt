a9104c4077faa9f03c2a75ac51f8b2b89e774bd9
jeda ngoding <46700670+savakun12@users.noreply.github.com>
2024-10-06 21:07:13 +0700

    Create kotlin.kt

---
+++ kotlin.kt
@@ -0,0 +1,14 @@
+## Hello World
+fun main() {
+    println("Hello, World!")
+}
+
+## Variabel
+val name: String = "Alice"
+var age: Int = 25
+
+## Function
+fun tambah(a: Int, b: Int): Int {
+  return a + b
+}
+
